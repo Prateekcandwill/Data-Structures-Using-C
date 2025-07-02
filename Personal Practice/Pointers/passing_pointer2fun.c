300ec4bff2120d3c293b1467ce741845a44bed4c
Prateek Kumar <93827361+Prateekcandwill@users.noreply.github.com>
2025-07-02 23:58:22 +0530

    Create passing_pointer2fun.c

---
+++ Personal Practice/Pointers/passing_pointer2fun.c
@@ -0,0 +1,30 @@
+Program-12: Passing a Pointer to a Function*/
+
+
+void FindAbs (int *pnum)
+
+{(if (*pnum < 0)
+
+/*Address received by a pointer/
+
+*pnum= *pnum (-1): /*Value accessed thru pointer */
+}
+
+void main()
+
+{ int x;
+
+printf("\nEnter value: ");
+scanf("d", &x);
+FindAbs (4x):
+
+/*Address of variable x passed /
+
+printf("\nThe required absolute value is d", x);
+}
+
+/*Output
+
+Enter value: -20
+
+The required absolute value is = 20*/
